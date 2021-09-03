acf1e6b1b4ab51b68a9640bd2b5aabf304f2eaf9
gkum4 <gustavokumasawa@gmail.com>
2021-09-03 11:06:31 -0300

    Initial Commit

---
+++ git_in_xcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_in_xcode
+//
+//  Created by Gustavo Kumasawa on 03/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git_in_xcode/git_in_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_in_xcodeApp.swift
+//  git_in_xcode
+//
+//  Created by Gustavo Kumasawa on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct git_in_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
